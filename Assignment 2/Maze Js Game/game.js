cd4f96bff009d0b400f8fdf4513ba45a5b74f94a
samerelearning <samerelearning@outlook.com>
2022-09-18 19:30:13 +0300

    Added documentation header

---
+++ Assignment 2/Maze Js Game/game.js
@@ -0,0 +1,8 @@
+/*************************************************************
+ * game.js
+ * @author Samer Saber
+ * This code's purpose is to access html elements using DOM only,
+ * and write event handling functions to make the html maze interactive.
+ * Other files include: game.html, game.css
+ * Last modified on Sunday, 18 of September 2022
+ *************************************************************/
