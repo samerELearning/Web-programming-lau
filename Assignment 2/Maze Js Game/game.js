c5d6be4a13d7155967d47534427bd43dcb954b10
samerelearning <samerelearning@outlook.com>
2022-09-18 20:01:02 +0300

    implementing red color on first border

--- Assignment 2/Maze Js Game/game.js
+++ Assignment 2/Maze Js Game/game.js
@@ -6,3 +6,13 @@
  * Other files include: game.html, game.css
  * Last modified on Sunday, 18 of September 2022
  *************************************************************/
+
+window.onload = function() {
+    const boundary = document.getElementById("boundary1");
+    boundary.onmouseover = touchBoundary;
+}
+
+function touchBoundary() {
+    const boundary = document.getElementById("boundary1");
+    boundary.style.backgroundColor = "#ff8888";
+}
