854e84855a464583d0954fe4d2759fe2c5532b8f
samerelearning <samerelearning@outlook.com>
2022-09-19 11:26:29 +0300

    fixed score additions

--- Assignment 2/Maze Js Game/game.js
+++ Assignment 2/Maze Js Game/game.js
@@ -9,6 +9,7 @@
 
 let message;//Stores messages that will show on the page
 let score;//keeps track of the score
+let touch;//Keeps track of number of times the boundaries are touched
 
 const boundaries = document.getElementsByClassName("boundary");
 const example_boundary = document.getElementsByClassName("boundary example");
@@ -17,17 +18,28 @@ window.onload = function() {
 
     message = document.getElementById("status");
     score   = 0;
+    touch   = 0;
 
     for (var i = 0; i < boundaries.length - 1; i++)
     {
         boundaries[i].onmouseover = touchBoundary;
     }
 }
-/*
-    This function is called when boundaries are touched, while moving the cursor
-    from start to end.
-*/
+
+/**
+ * This function is called when boundaries are touched, while moving the cursor
+ * from start to end.
+ */
 function touchBoundary() {
+
+    touch++;
+
+    if (touch < 2)
+    {
+    //Only decreases score if boundaries are touched once
+        score -= 10;
+
+    }
 
     for (var i = 0; i < boundaries.length - 1; i++)
     {
@@ -35,7 +47,6 @@ function touchBoundary() {
         boundaries[i].style.backgroundColor = "#ff8888";
     }
 
-    score -= 10;
     message.innerText = "You lost! Score: " + score;
 
 }
