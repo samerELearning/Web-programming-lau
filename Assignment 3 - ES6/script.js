c4a03ed9ce0531547256c006b1fc6c53af253d03
samerelearning <samerelearning@outlook.com>
2022-10-01 06:15:46 +0300

    starting with javascript

---
+++ Assignment 3 - ES6/script.js
@@ -0,0 +1,9 @@
+/*************************************************************
+ * script.js
+ * @author Samer Saber
+ * This code's purpose is to implement a tic tac toe game,
+ * and a minimax algorithm in order to play against a smart computer.
+ * Other files include: index.html, syle.css
+ * Last modified on Saturday, 1st of October 2022
+ *************************************************************/
+
