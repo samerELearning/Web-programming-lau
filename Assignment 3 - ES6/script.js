415e09926cefdce416813fb24895bf69c19a60cf
samerelearning <samerelearning@outlook.com>
2022-10-02 23:04:34 +0300

    implemented the hasWon() function

--- Assignment 3 - ES6/script.js
+++ Assignment 3 - ES6/script.js
@@ -12,10 +12,10 @@ let user_score;//Keeps track of user score
 let ai_score;//Keeps track of AI's score
 let turn;//Keeps track of player's turn
 
+const board  = [1,2,3,4,5,6,7,8,9];//Keeps track of board cells
+
 const tokens = document.getElementsByTagName("img");
 const cells  = document.getElementsByClassName("cell");
-
-const board  = [1,2,3,4,5,6,7,8,9];
 
 window.onload = function() {
     
@@ -60,10 +60,10 @@ function restartGame()
 }
 
 /**
- * This function is called
+ * This function is called inside 'minimax()'.
  * The purpose of this function is to return a list
  * of empty board cells.
- * @param {List} board The list of board cells
+ * @param   {List} board The list of board cells
  * @returns {List}     The list of empty board cells
  */
 function emptyCells(board)
@@ -71,3 +71,52 @@ function emptyCells(board)
     return board.filter(c => c != "y" && c != "r");
 }
 
+/**
+ * This function is called inside 'minimax()'.
+ * The purpose of this function is to determine
+ * if the player passed in the parameter has won
+ * the game or not, by checking all winning combinations.
+ * @param   {List}   board  The list of board cells
+ * @param   {String} player 'y' for AI or 'r' for human
+ * @returns {boolean}       True if won, false otherwise
+ */
+function hasWon(board, player)
+{
+    if ((board[0] == player && board[1] == player && board[2] == player) ||
+        (board[3] == player && board[4] == player && board[5] == player) ||
+        (board[6] == player && board[7] == player && board[8] == player) ||
+        (board[0] == player && board[3] == player && board[6] == player) ||
+        (board[1] == player && board[4] == player && board[7] == player) ||
+        (board[2] == player && board[5] == player && board[8] == player) ||
+        (board[0] == player && board[4] == player && board[8] == player) ||
+        (board[2] == player && board[4] == player && board[6] == player))
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+
+function minimax(new_board, player)
+{
+    const empty_cells = emptyCells(new_board);
+
+    if (hasWon(new_board, "r"))
+    {
+    //If human wins
+        return -10;
+    }
+    else if (hasWon(new_board, "y"))
+    {
+    //If AI wins
+        return 10;
+    }
+    else if (empty_cells.length === 0)
+    {
+    //If there's a tie
+        return 0;
+    }
+}
