41dcf1ccc77b0eca5e46dd0775033bccb41a9508
samerelearning <samerelearning@outlook.com>
2022-10-03 05:24:21 +0300

    updating modification date in documentation

--- Assignment 3 - ES6/script.js
+++ Assignment 3 - ES6/script.js
@@ -4,7 +4,7 @@
  * This code's purpose is to implement a tic tac toe game,
  * and a minimax algorithm in order to play against a smart computer.
  * Other files include: index.html, syle.css
- * Last modified on Saturday, 1st of October 2022
+ * Last modified on Saturday, 2nd of October 2022
  *************************************************************/
 
 let message;//Stores message that will show on page
