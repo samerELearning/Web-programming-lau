54e8959200e186e162d1dd620b670dab646872f2
samerelearning <samerelearning@outlook.com>
2022-10-03 04:58:09 +0300

    implemented winning conditions

--- Assignment 3 - ES6/script.js
+++ Assignment 3 - ES6/script.js
@@ -8,9 +8,11 @@
  *************************************************************/
 
 let message;//Stores message that will show on page
+let score;//Holds 'score' element in html file
 let user_score;//Keeps track of user score
 let ai_score;//Keeps track of AI's score
 let turn;//Keeps track of player's turn
+let start;//Stores '1' when game first starts
 
 const board  = [0,1,2,3,4,5,6,7,8];//Keeps track of board cells
 
@@ -20,36 +22,64 @@ const cells  = document.getElementsByClassName("cell");
 window.onload = function() {
     
     message     = document.getElementById("message");
+    score       = document.getElementById("score");
     turn        = 1;
     user_score  = 0;
     ai_score    = 0;
+    start       = 0;
 
 }
 
 /**
  * This function is called from the html file.
  * The purpose of this function is to set the token image
- * to visible on the cell that was clicked by the user.
+ * to visible on the cell that was clicked by the user,
+ * and to call the function 'aiPlaceToken(index)' to determine
+ * and place AI's move.
  * @param {Integer} index The index of the cell
  */
 function placeToken(index)
 {
-    if (turn)
+    if (turn && board[index] != "r" && board[index] != "y")
     {
         tokens[index + 1].style.visibility = "visible";
         board[index]                       = "r";
         turn                               = 0;
-    }
+        start                              = 1;
 
-    console.log(minimax(board, "y").index);/////////////////////////////////////////////////////////////////////////
+        
+        if (board.filter(c => c != "y" && c != "r").length == 0)
+        {
+        //If no more empty cells, then it's a tie
+            ai_score         += 1;
+            user_score       += 1;
+            message.innerText = "It's a tie!";
+            score.innerText   = "Your score: " + user_score + "\n" + "Computer's score: " + ai_score;
+        }
+        else
+        {
+            aiPlaceToken(minimax(board, "y").index);
+        }
+    }
 }
+
 
 function aiPlaceToken(index)
 {
-    tokens[index].src              = "img/yellow.png";
-    tokens[index].style.visibility = "visible";
-    board[index]                   = "y";
-    turn                           = 1;
+    tokens[index + 1].src              = "img/yellow.png";
+    tokens[index + 1].style.visibility = "visible";
+    board[index]                       = "y";
+
+    if (hasWon(board, "y"))
+    {
+        ai_score         += 1;
+        message.innerText = "Computer Wins!";
+        score.innerText   = "Your score: " + user_score + "\n" + "Computer's score: " + ai_score;
+    }
+    else
+    {
+        turn = 1;
+    }
 }
 
 /**
@@ -59,16 +89,22 @@ function aiPlaceToken(index)
  */
 function restartGame()
 {
-    for (var i = 1; i < tokens.length; i++)
+    if (start)
     {
-        tokens[i].style.visibility = "hidden";
-        board[i - 1]               = i - 1;
+    //If game started
+        for (var i = 1; i < tokens.length; i++)
+        {
+            tokens[i].style.visibility = "hidden";
+            tokens[i].src              = "img/red.png";
+            board[i - 1]               = i - 1;
+        }
+
+        board[8] = 8;
+
+        message.innerText   = "Let's Play Again!";
+        turn                = 1;
+        start               = 0;
     }
-
-    board[8] = 8;
-
-    message.innerText   = "Let's Play Again!";
-    turn                = 1;
 }
 
 /**
